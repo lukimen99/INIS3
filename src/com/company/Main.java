2bf539c976770a7aae591f3911464d1d09ba1ede
lukimen99 <bodziothat@gmail.com>
2021-02-26 17:18:27 +0100

    tk

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Animal dog = new Animal();
+        dog.species = "dog";
+        dog.name = "Haiku";
+        dog.weight = 8.0;
+
+        Phone iPhone = new Phone();
+        iPhone.screenSize = 4.5;
+        iPhone.producer = "Apple";
+    }
+}
